a7e303136e4dc595c947c6e935a8353caa28a323
Usanezumi <117045598+Usanezumi@users.noreply.github.com>
2022-11-04 01:41:16 +0900

    Update server.js

--- bbs-master/server.js
+++ bbs-master/server.js
@@ -13,7 +13,7 @@ if (user && pass) {
 
 app.use(express.logger('dev'));
 app.use(express.compress());
-app.use(express.static(__dirname + '/templates'));
+app.use(express.static(__dirname + '/templates/'));
 
 app.listen(app.get('port'), function() {
   console.log('Server listening on port %s', app.get('port'));
